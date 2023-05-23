d4743e587058e3610025163143fb872856658904
Gmurad735 <abd.gm90@yandex.ru>
2023-05-23 20:32:25 +0300

    add manhattan

--- my_code.py
+++ my_code.py
@@ -1,9 +1,13 @@
-def dist(point_1, point_2):
+def dist(point_1, point_2, manhattan=False):
     (x1, y1), (x2, y2) = point_1, point_2
+
+    if manhattan:
+        return abs(x1-x2) + abs(y1-y2)
+
     return ((x1-x2)**2 + (y1-y2)**2)**0.5
 
 def main():
-    print(f"{dist((0, 0), (1, 1)):.3f}")
+    print(f"{dist((0, 0), (1, 1), manhattan=True):.3f}")
 
 if __name__=="__main__":
     main()
