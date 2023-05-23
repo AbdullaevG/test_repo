b03b62af221f4f8722d2faf9c9f3a7fd55ddd3b4
Gmurad735 <abd.gm90@yandex.ru>
2023-05-23 20:25:39 +0300

    add comments

--- my_code.py
+++ my_code.py
@@ -1,9 +1,15 @@
 def dist(point_1, point_2):
+    """
+    This function calculate the euclidean
+    distance from point_1 to point_2 in 2D space
+    """
     (x1, y1), (x2, y2) = point_1, point_2
     return ((x1-x2)**2 + (y1-y2)**2)**0.5
 
+
 def main():
     print(f"{dist((0, 0), (1, 1)):.3f}")
+
 
 if __name__=="__main__":
     main()
